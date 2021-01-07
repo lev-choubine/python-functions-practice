d68cb92ab2a4c0a3ca9f5d450a81fd47d128b332
Lev Michael <lev.choubine@gmail.com>
2021-01-07 16:25:54 -0500

    no fizzbuzz

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,12 +1,12 @@
 data = int(input("enter a number "))
 
 def fizzbuzz(n):
-    for i in range(n):
+    for i in range(n+1):
         if i % 3 == 0:
             print('fizz')
         elif i % 5 == 0:
             print('buzz')
-        elif i % 3 and i % 5 == 0:
+        elif i % 3 == 0 and i % 5 == 0:
             print('fizzbuzz')
         else: print(i)    
 
