a71cab9d8abde7138ac3b4043e380e8fcbe95266
Lev Michael <lev.choubine@gmail.com>
2021-01-07 16:30:24 -0500

    fizzbuzz

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -2,12 +2,26 @@ data = int(input("enter a number "))
 
 def fizzbuzz(n):
     for i in range(n+1):
-        if i % 3 == 0:
+        if i % 3 == 0 and i % 5 == 0:
+            print('fizzbuzz')
+        elif i % 3 == 0:
             print('fizz')
         elif i % 5 == 0:
             print('buzz')
-        elif i % 3 == 0 and i % 5 == 0:
-            print('fizzbuzz')
         else: print(i)    
 
 fizzbuzz(data)
+
+"""
+function oddRange(end) {
+  const arr = [];
+
+  for (let i = 1; i <= end; i += 2) {
+      arr.push(i);
+  }
+
+  return arr;
+}
+"""
+def oddRange(n):
+    list = []
